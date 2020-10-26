35efd339fbd6cd5072800ec75efc2d4ca595478e
sady410 <sachaco@hotmail.fr>
2020-10-26 10:41:57 +0100

    client with mapbox-gl setup

---
+++ client/app/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let version;
+	import Map from "./components/Map.svelte";
+	import MapMarker from './components/MapMarker.svelte';
+</script>
+
+<main>
+
+	<h1>Projet Interdisciplinaire: v{version}</h1>
+
+	<div class='mapbox'>
+		<Map lat={50.4667} lon={4.8667} zoom={14.5}>
+			<MapMarker lat={50.465856} lon={4.857599} label="UNamur"/>
+		</Map>
+	</div>
+
+</main>
+
+<style>
+
+
+	.mapbox {
+		height: 75vh;
+		margin: 1rem;
+	}
+
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ client/app/src/lib/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "pk.eyJ1Ijoic2FkeTQxMCIsImEiOiJja2dxY2RpNTIxbHI3MnhyeHNmemZ6Znk5In0.xaV73rCTkiwmljImKPsxoQ";
+
+const key = {};
+
+export { mapbox, key };
