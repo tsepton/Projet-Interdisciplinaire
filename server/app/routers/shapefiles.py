00b1144b10e72a347c524e5caaee893fd72cf9c6
sady410 <sachaco@hotmail.fr>
2020-10-28 18:20:51 +0100

    add shapefiles routes

--- server/app/routers/shapefiles.py
+++ server/app/routers/shapefiles.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import os.path
+import json
 
 router = APIRouter()
+data_path = "data/shapefiles/"
+
+@router.get("/shapefiles/{file}/")
+async def read_shapefiles(file: str):
+    file += '.geojson'
+    if os.path.isfile(data_path + file):
+        with open(data_path + file, newline='') as shapefile:
+            return json.load(shapefile)
+    else:
+        raise HTTPException(status_code=404, detail="Shapefile not found")
