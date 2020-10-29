0a0f788c42834daf3c90fb5b609e875b3aefbbf5
Thibaut Septon <tsepton@gmail.com>
2020-10-29 15:10:57 +0100

    Add cors origins

--- server/app/main.py
+++ server/app/main.py
@@ -2,6 +2,7 @@ import os
 import ptvsd
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import gtfs, shapefiles
 
 """
@@ -11,6 +12,17 @@ if os.environ['ENVIRONMENT'] != 'PROD':
     ptvsd.enable_attach()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080",
+        "http://localhost:5000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(gtfs.router)
 app.include_router(shapefiles.router)
